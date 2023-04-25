dde8efd4d18fa88a9cd53fca736bb78c270f2337
Pourya Erfanzadeh <erfnzdeh@gmail.com>
2023-04-25 14:18:46 +0330

    minor changes.

--- stronghold/src/main/java/stronghold/model/StrongHold.java
+++ stronghold/src/main/java/stronghold/model/StrongHold.java
@@ -25,9 +25,9 @@ public class StrongHold {
 		}
 		return null;
 	}
-	public static void setCurentUser(String username)
+	public static void setCurentUser(User user)
 	{
-		currentUser = getUserByName(username);
+		currentUser = user;
 	}
 	public static User getCurrentUser() {
 		return currentUser;
