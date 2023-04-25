661b458f4632851eff6de0525fca4df5077c6eb8
sinamhdv <sinamhdv@gmail.com>
2023-04-25 23:40:13 +0330

    added Person class

--- stronghold/src/main/java/stronghold/model/people/Person.java
+++ stronghold/src/main/java/stronghold/model/people/Person.java
@@ -1,5 +1,55 @@
 package stronghold.model.people;
 
+import stronghold.model.Government;
+
 public abstract class Person {
+	private int speed;
+	private int hp;
+	private int damage;
+	private final Government owner;
+	private int x;
+	private int y;
 	
+	public Person(int speed, int hp, int damage, Government owner, int x, int y) {
+		this.speed = speed;
+		this.hp = hp;
+		this.damage = damage;
+		this.owner = owner;
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	public int getDamage() {
+		return damage;
+	}
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	public Government getOwner() {
+		return owner;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
 }
