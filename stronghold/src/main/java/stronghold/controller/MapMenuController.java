4e5adc977c98a8af2c13d6c22bcfb6131734b1d8
sinamhdv <sinamhdv@gmail.com>
2023-04-26 00:13:20 +0330

    working on map menu

--- stronghold/src/main/java/stronghold/controller/MapMenuController.java
+++ stronghold/src/main/java/stronghold/controller/MapMenuController.java
@@ -1,5 +1,10 @@
 package stronghold.controller;
 
 public class MapMenuController {
-	
+	private static final int SHOW_MAP_WIDTH = 10;
+	private static final int SHOW_MAP_HEIGHT = 5;
+
+	public static void moveMap(String up, String down, String left, String right) {
+		// TODO
+	}
 }
--- stronghold/src/main/java/stronghold/view/MapMenu.java
+++ stronghold/src/main/java/stronghold/view/MapMenu.java
@@ -2,6 +2,10 @@ package stronghold.view;
 
 import java.util.HashMap;
 
+import stronghold.controller.MapMenuController;
+import stronghold.model.StrongHold;
+import stronghold.model.map.MapTile;
+import stronghold.utils.FormatValidation;
 import stronghold.view.parser.Command;
 import stronghold.view.parser.CommandParser;
 
@@ -24,5 +28,36 @@ public class MapMenu {
 		}
 	}
 
-	
+	public static void runShowMap(HashMap<String, String> matcher) {
+		String xString = matcher.get("x");
+		String yString = matcher.get("y");
+		if (xString == null || yString == null) {
+			System.out.println("Error: please specify both x and y");
+			return;
+		}
+		if (!FormatValidation.isNumber(xString) || !FormatValidation.isNumber(yString)) {
+			System.out.println("Error: invalid numeric argument entered.");
+			return;
+		}
+		showMap(Integer.parseInt(xString), Integer.parseInt(yString));
+	}
+
+	public static void runMoveMap(HashMap<String, String> matcher) {
+		for (String key : matcher.keySet()) {
+			if (!FormatValidation.isNumber(matcher.get(key))) {
+				System.out.println("Error: invalid numeric argument entered.");
+				return;
+			}
+		}
+		MapMenuController.moveMap(matcher.get("up"), matcher.get("down"), matcher.get("left"), matcher.get("right"));
+	}
+
+	public static void runShowTileDetails(HashMap<String, String> matcher) {
+		// TODO
+	}
+
+	public static void showMap(int x, int y) {
+		MapTile[][] map = StrongHold.getCurrentGame().getMap();
+		// TODO
+	}
 }
