ae30a2bcbd8d14dcf8b80228c0df9561e9978269
sinamhdv <sinamhdv@gmail.com>
2023-04-25 23:58:25 +0330

    implemented map menu run function

--- stronghold/src/main/java/stronghold/view/MapMenu.java
+++ stronghold/src/main/java/stronghold/view/MapMenu.java
@@ -1,5 +1,28 @@
 package stronghold.view;
 
+import java.util.HashMap;
+
+import stronghold.view.parser.Command;
+import stronghold.view.parser.CommandParser;
+
 public class MapMenu {
+	public static void run() {
+		while (true) {
+			String[] input = CommandParser.splitTokens(MainMenu.getScanner().nextLine());
+			HashMap<String, String> matcher;
+
+			if ((matcher = CommandParser.getMatcher(input, Command.SHOW_MAP)) != null)
+				runShowMap(matcher);
+			else if ((matcher = CommandParser.getMatcher(input, Command.SHOW_TILE_DETAILS)) != null)
+				runShowTileDetails(matcher);
+			else if ((matcher = CommandParser.getMatcher(input, Command.MOVE_MAP)) != null)
+				runMoveMap(matcher);
+			else if ((matcher = CommandParser.getMatcher(input, Command.BACK)) != null)
+				return;
+			else
+				System.out.println("invalid command");
+		}
+	}
+
 	
 }
