295d51906374b34fc6261b0434beb8795149766f
Pourya Erfanzadeh <erfnzdeh@gmail.com>
2023-04-25 11:36:50 +0330

    LoginMenuController.java work in progress.

---
+++ stronghold/src/main/java/stronghold/controller/LoginMenuController.java
@@ -0,0 +1,40 @@
+package stronghold.controller;
+
+import stronghold.controller.messages.LoginMenuControllerMessage;
+import stronghold.model.StrongHold;
+
+import static java.lang.Thread.*;
+
+public class LoginMenuController {
+    //TODO: 5 second punishments need to be implemented.
+    static int timer = 1;
+
+    public static void LoginPunishment() throws InterruptedException {
+        timer++;
+        System.out.println("You have to wait for " + timer * 5 + " seconds before you can login again!");
+        sleep(5000L * timer);
+        System.out.println("You're free!");
+    }
+
+    public static LoginMenuControllerMessage login(String username, String password, String s) {
+        if (StrongHold.getUserByName(username) == null) {
+            return LoginMenuControllerMessage.USERNAME_ERROR; //i need help here.
+        } else if (StrongHold.getUserByName(username) != null /*&& password.equals(StrongHold.getUserByName(username).getPassword())*/) {
+            return LoginMenuControllerMessage.PASSWORD_ERROR;
+        } else {
+            StrongHold.setCurentUser(username);
+            return LoginMenuControllerMessage.SUCCESSFUL;
+//            MainMenu.run();
+        }
+    }
+
+    public static LoginMenuControllerMessage forgotPassword(String username) {
+        if (StrongHold.getUserByName(username) == null) {
+            System.out.println(LoginMenuControllerMessage.USERNAME_NOT_FOUND);
+        } else {
+            User user = StrongHold.getUserByName(username);
+
+
+        }
+    }
+}
