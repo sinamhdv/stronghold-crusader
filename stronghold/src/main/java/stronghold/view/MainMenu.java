835b54db1de7ca6213e4a2930d278f5226565691
Pourya Erfanzadeh <erfnzdeh@gmail.com>
2023-04-25 11:44:22 +0330

    getScanner() was added.

--- stronghold/src/main/java/stronghold/view/MainMenu.java
+++ stronghold/src/main/java/stronghold/view/MainMenu.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class MainMenu {
 	private static final Scanner scanner = new Scanner(System.in);
-	
-	
+	public static Scanner getScanner() {
+		return scanner;
+	}
 }
