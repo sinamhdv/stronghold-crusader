0d98aa5bfd8f1fb810ffb7113a6059fbcae88316
sinamhdv <sinamhdv@gmail.com>
2023-04-29 17:54:31 +0330

    added name to GroundType

--- stronghold/src/main/java/stronghold/model/map/GroundType.java
+++ stronghold/src/main/java/stronghold/model/map/GroundType.java
@@ -3,15 +3,17 @@ package stronghold.model.map;
 import stronghold.view.TerminalColor;
 
 public enum GroundType {
-	NORMAL(true, TerminalColor.BROWN),
+	NORMAL(true, TerminalColor.BROWN, "normal"),
 	;
 
 	private final boolean isPassable;
 	private final TerminalColor color;
+	private final String name;
 
-	private GroundType(boolean isPassable, TerminalColor color) {
+	private GroundType(boolean isPassable, TerminalColor color, String name) {
 		this.isPassable = isPassable;
 		this.color = color;
+		this.name = name;
 	}
 
 	public TerminalColor getColor() {
@@ -19,5 +21,8 @@ public enum GroundType {
 	}
 	public boolean isPassable() {
 		return isPassable;
+	}
+	public String getName() {
+		return name;
 	}
 }
