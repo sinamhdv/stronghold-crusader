47f5a3e738461bf0d4402f255e6b6685569f5e4a
sinamhdv <sinamhdv@gmail.com>
2023-04-30 18:43:54 +0330

    added environment items to map tiles

--- stronghold/src/main/java/stronghold/model/map/MapTile.java
+++ stronghold/src/main/java/stronghold/model/map/MapTile.java
@@ -3,12 +3,14 @@ package stronghold.model.map;
 import java.util.ArrayList;
 
 import stronghold.model.buildings.Building;
+import stronghold.model.environment.EnvironmentItem;
 import stronghold.model.people.Person;
 
 public class MapTile {
 	private GroundType groundType;
-	private ArrayList<Person> people = new ArrayList<>();
+	private final ArrayList<Person> people = new ArrayList<>();
 	private Building building = null;
+	private EnvironmentItem environmentItem;
 
 	public MapTile(GroundType groundType) {
 		this.groundType = groundType;
@@ -28,5 +30,11 @@ public class MapTile {
 	}
 	public void setGroundType(GroundType groundType) {
 		this.groundType = groundType;
-	}	
+	}
+	public EnvironmentItem getEnvironmentItem() {
+		return environmentItem;
+	}
+	public void setEnvironmentItem(EnvironmentItem environmentItem) {
+		this.environmentItem = environmentItem;
+	}
 }
