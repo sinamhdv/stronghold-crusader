00b68eaff54f1458ddfbb64ff323b0bdb6401437
sinamhdv <sinamhdv@gmail.com>
2023-04-30 20:10:01 +0330

    debugged terminal 2D printer

--- stronghold/src/main/java/stronghold/view/Terminal2DPrinter.java
+++ stronghold/src/main/java/stronghold/view/Terminal2DPrinter.java
@@ -14,14 +14,16 @@ public class Terminal2DPrinter {
 		// add as many lines as necessary
 		while (currentLine + output.length - 1 >= outputArray.size())
 			outputArray.add("");
-		
+
+		// pad bottom lines to match the size of current line
 		for (int i = 0; i < output.length; i++) {
-			// pad bottom lines to match the size of current line
 			while (outputArray.get(currentLine + i).length() < outputArray.get(currentLine).length())
-				outputArray.set(i, outputArray.get(i) + " ");
-			// add one line of output
-			outputArray.set(i, outputArray.get(i) + output[i]);
+				outputArray.set(currentLine + i, outputArray.get(currentLine + i) + " ");
 		}
+
+		// add one line of output
+		for (int i = 0; i < output.length; i++)
+			outputArray.set(currentLine + i, outputArray.get(currentLine + i) + output[i]);
 	}
 
 	public void addNewLine() {
