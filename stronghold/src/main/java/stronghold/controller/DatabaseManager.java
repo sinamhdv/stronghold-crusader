99336b7a27e7735c16484dfba93705d8bf1b4cc5
sinamhdv <sinamhdv@gmail.com>
2023-04-30 23:49:50 +0330

    added writeToFile and readFromFile functions to DatabaseManager

--- stronghold/src/main/java/stronghold/controller/DatabaseManager.java
+++ stronghold/src/main/java/stronghold/controller/DatabaseManager.java
@@ -1,6 +1,7 @@
 package stronghold.controller;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,22 +17,33 @@ import stronghold.model.map.MapTile;
 public class DatabaseManager {
 	
 	private static final String USERS_DATABASE_FILENAME = "stronghold/src/main/database/users.json";
+	private static final String STAY_LOGGED_IN_FILENAME = "stronghold/src/main/database/stay-logged-in.txt";
+
+	private static void writeToFile(String filename, String content) {
+		File file = new File(filename);
+		try {
+			FileUtils.writeStringToFile(file, content, Charset.forName("UTF-8"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String readAllFromFile(String filename) {
+		File file = new File(filename);
+		if (!file.exists()) return null;
+		try {
+			return FileUtils.readFileToString(file, Charset.forName("UTF-8"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 	public static void loadUsers() {
-		File file = new File(USERS_DATABASE_FILENAME);
-		if (!file.exists()) {
-			StrongHold.setUsers(new ArrayList<>());
-			return;
-		}
-		try {
-			String jsonData = FileUtils.readFileToString(file, Charset.forName("UTF-8"));
-			Gson gson = new Gson();
-			User[] usersArray = gson.fromJson(jsonData, User[].class);
-			StrongHold.setUsers(new ArrayList<>(Arrays.asList(usersArray)));
-		}
-		catch (Exception e) {
-			StrongHold.setUsers(new ArrayList<>());
-		}
+		String jsonData = readAllFromFile(USERS_DATABASE_FILENAME);
+		Gson gson = new Gson();
+		User[] usersArray = (jsonData == null ? new User[0] : gson.fromJson(jsonData, User[].class));
+		StrongHold.setUsers(new ArrayList<>(Arrays.asList(usersArray)));
 	}
 
 	public static void updateUser(User user) {
@@ -41,13 +53,11 @@ public class DatabaseManager {
 	private static void saveUsers() {
 		Gson gson = new Gson();
 		String jsonData = gson.toJson(StrongHold.getUsers());
-		File file = new File(USERS_DATABASE_FILENAME);
-		try {
-			FileUtils.writeStringToFile(file, jsonData, Charset.forName("UTF-8"));
-		}
-		catch (Exception e) {
-			System.out.println("Error while saving the database");
-		}
+		writeToFile(USERS_DATABASE_FILENAME, jsonData);
+	}
+
+	public static void saveStayLoggedIn(User user) {
+
 	}
 
 	public static void saveMap(MapTile[][] map) {
