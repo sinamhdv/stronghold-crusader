8843b89f0c2ea18fc8516f2eda36fd7f448569db
mobinakochaknia <mobina.kochaknia@yahoo.com>
2023-05-05 18:25:09 +0430

    create AsciiArtGenerator

---
+++ stronghold/src/main/java/stronghold/view/cap/AsciiArtGenerator.java
@@ -0,0 +1,12 @@
+package stronghold.view.captcha;
+
+import java.util.Random;
+
+public class AsciiArtGenerator {
+	private String[] getAsciiArtForOneNummber {
+		Random random = new Random();
+		int number = random.nextInt(9);
+		
+	}
+	
+}
