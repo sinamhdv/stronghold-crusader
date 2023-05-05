664f35f9069b9977d206136ef2ead0a8f2419cd4
mobinakochaknia <mobina.kochaknia@yahoo.com>
2023-05-05 21:44:24 +0430

    added CreateCaptchaAsciiArt

---
+++ stronghold/src/main/java/stronghold/view/captcha/AsciiArtGenerator.java
@@ -0,0 +1,28 @@
+package stronghold.view.captcha;
+
+import java.util.Random;
+
+import stronghold.controller.DatabaseManager;
+
+public class AsciiArtGenerator {
+	private static String[] getAsciiArtOfRandomNumber() {
+		String[][] digitAsciiArt = DatabaseManager.loadCaptchaAsciiArt();
+		Random random = new Random();
+		int number = random.nextInt(9);
+		return digitAsciiArt[number];
+	}
+
+	public static String[] createCaptchaAsciiArt() {
+		String[][] oneDigitOfCaptcha = new String[4][6];
+		String[] captcha = new String[6];
+		for (int i = 0; i < 4; i++) {
+			oneDigitOfCaptcha[i] = getAsciiArtOfRandomNumber();
+		}
+		for (int i = 0; i < 6; i++) {
+			captcha[i] += oneDigitOfCaptcha[0][i] + oneDigitOfCaptcha[1][i] + oneDigitOfCaptcha[2][i]
+					+ oneDigitOfCaptcha[3][i];
+		}
+		return captcha;
+	}
+
+}
