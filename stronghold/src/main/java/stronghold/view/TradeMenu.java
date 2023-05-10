fa30fb6a928474e8e952ad23059b91a55edd7d99
mobinakochaknia <mobina.kochaknia@yahoo.com>
2023-05-10 12:40:44 +0430

    added Trade accept to Show trade list

--- stronghold/src/main/java/stronghold/view/TradeMenu.java
+++ stronghold/src/main/java/stronghold/view/TradeMenu.java
@@ -2,11 +2,13 @@ package stronghold.view;
 
 import stronghold.model.Game;
 import stronghold.model.StrongHold;
+import stronghold.model.TradeAccept;
 import stronghold.model.TradeRequest;
 
 public class TradeMenu {
 	public static void showTradeList() {
 		Game currentGame = StrongHold.getCurrentGame();
+		System.out.println("===============================Trade Request===================================");
 		System.out.println("User     Resourcename     Amount     Price     Id     message");
 		for (TradeRequest trade : currentGame.getAllTrads()) {
 			System.out.println(
@@ -14,7 +16,13 @@ public class TradeMenu {
 							+ trade.getAmount() + "     " + trade.getPrice() + "     " + trade.getId() + "     "
 							+ trade.getMessage());
 		}
+		System.out.println("===============================Trade Accept===================================");
+		System.out.println("request by     Accept by     Trade Id     message");
+		for (TradeAccept tradeAccept : currentGame.getAllTradeAccepts()) {
+			System.out.println(tradeAccept.getRequestBy().getUser().getNickName() + "     "
+					+ tradeAccept.getAcceptby().getUser().getNickName() + "     " + tradeAccept.getTradeId() + "     "
+					+ tradeAccept.getMessage());
+		}
 	}
 
-	
 }
