d5b36c972e1db212ed21453a7b8b4a400c714646
sinamhdv <sinamhdv@gmail.com>
2023-05-10 22:51:08 +0330

    added show popularity

--- stronghold/src/main/java/stronghold/controller/GameMenuController.java
+++ stronghold/src/main/java/stronghold/controller/GameMenuController.java
@@ -1,5 +1,10 @@
 package stronghold.controller;
 
+import stronghold.model.Game;
+
 public class GameMenuController {
-	
+	private static Game game;
+	public static void setGame(Game game) {
+		GameMenuController.game = game;
+	}
 }
