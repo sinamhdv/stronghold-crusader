bad41de90edf8aee0b8ab9ad057663acbd9cb9bc
sinamhdv <sinamhdv@gmail.com>
2023-05-28 19:19:10 +0330

    modified unit-tests to be compatible with the new signup function

--- stronghold/src/test/java/stronghold/AppTest.java
+++ stronghold/src/test/java/stronghold/AppTest.java
@@ -16,24 +16,24 @@ public class AppTest {
 	public void shouldAnswerWithTrue() {
 		assertTrue(true);
 	}
-/*
+
 	@Test
 	public void testSignUpError() {
-		SignupAndProfileMenuMessage erorr = SignupMenuController.signup("", "", "lk4ld", "hdfyh@hghd.com", "");
+		SignupAndProfileMenuMessage erorr = SignupMenuController.signup("", "", "lk4ld", "hdfyh@hghd.com", "", 0, "");
 		assertEquals(erorr, SignupAndProfileMenuMessage.EMPTY_FIELD);
 	}
 
 	@Test
 	public void testSignCheckError() {
 		SignupAndProfileMenuMessage error = SignupMenuController.signUpFactorsError("gcdyuhg", "eywy",
-				"Mobina_Kochaknia34", "mobina.kochaknia@yahho.com", "hiiii");
+				"Mobina_Kochaknia34", "mobina.kochaknia@yahho.com", "hiiii", 1, "aaa");
 		assertNull(error);
 	}
 
 	@Test
 	public void testSignUpError2() {
 		SignupAndProfileMenuMessage error = SignupMenuController.signup("_)(*&*)", "eywy",
-				"Mobina_kochaknia34", "mobina.kochaknia@yahho.com", "hiiii");
+				"Mobina_kochaknia34", "mobina.kochaknia@yahho.com", "hiiii", 1, "aaa");
 		assertEquals(SignupAndProfileMenuMessage.INVALID_USERNAME, error);
 	}
 
@@ -42,49 +42,49 @@ public class AppTest {
 		StrongHold.getUsers().add(new User("gcdyuhg", "eywy",
 				"Mobina_Kochaknia34", "Mobina_Kochaknia34", "mobina.kochaknia@yahho.com", 0, 1, "hiiii"));
 		SignupAndProfileMenuMessage error = SignupMenuController.signUpFactorsError("gcdyuhg", "eywy",
-				"Mobina_Kochaknia34", "mobina.kochaknia@yahho.com", "hiiii");
+				"Mobina_Kochaknia34", "mobina.kochaknia@yahho.com", "hiiii", 1, "aaa");
 		assertEquals(SignupAndProfileMenuMessage.USERNAME_EXIST, error);
 	}
 
 	@Test
 	public void testSignUpError4() {
 		SignupAndProfileMenuMessage error = SignupMenuController.signup("gcdyuhg", "eywy",
-				"Mo", "mobina.kochaknia@yahho.com", "hiiii");
+				"Mo", "mobina.kochaknia@yahho.com", "hiiii", 1, "aaa");
 		assertEquals(SignupAndProfileMenuMessage.PASSWORD_IS_SHORT, error);
 	}
 
 	@Test
 	public void testSignUpError5() {
 		SignupAndProfileMenuMessage error = SignupMenuController.signup("gcdyuhg", "eywy",
-				"efwretwegtse", "mobina.kochaknia@yahho.com", "hiiii");
+				"efwretwegtse", "mobina.kochaknia@yahho.com", "hiiii", 1, "aaa");
 		assertEquals(SignupAndProfileMenuMessage.PASSWORD_DOSNT_HAVE_UPPERCASE, error);
 	}
 
 	@Test
 	public void testSignUpError6() {
 		SignupAndProfileMenuMessage error = SignupMenuController.signup("gcdyuhg", "eywy",
-				"HGVGHFYFY", "mobina.kochaknia@yahho.com", "hiiii");
+				"HGVGHFYFY", "mobina.kochaknia@yahho.com", "hiiii", 1, "aaa");
 		assertEquals(SignupAndProfileMenuMessage.PASSWORD_DOSNT_HAVE_LOWERCASE, error);
 	}
 
 	@Test
 	public void testSignUpError7() {
 		SignupAndProfileMenuMessage error = SignupMenuController.signup("gcdyuhg", "eywy",
-				"HGVGHFYFYjj", "mobina.kochaknia@yahho.com", "hiiii");
+				"HGVGHFYFYjj", "mobina.kochaknia@yahho.com", "hiiii", 1, "aaa");
 		assertEquals(SignupAndProfileMenuMessage.PASSWORD_DOSNET_HAVE_NUMBER, error);
 	}
 
 	@Test
 	public void testSignUpError8() {
 		SignupAndProfileMenuMessage error = SignupMenuController.signup("gcdyuhg", "eywy",
-				"HGxVG66HFYFY", "mobina.kochaknia@yahho.com", "hiiii");
+				"HGxVG66HFYFY", "mobina.kochaknia@yahho.com", "hiiii", 1, "aaa");
 		assertEquals(SignupAndProfileMenuMessage.PASSWORD_DOSENT_HAVE_OTHER_CHAR, error);
 	}
 
 	@Test
 	public void testSignUpError11() {
 		SignupAndProfileMenuMessage error = SignupMenuController.signup("gcdyuhg", "eywy",
-				"HGVG66__fcgHFYFY", "mobina.kochakniayahho.com", "hiiii");
+				"HGVG66__fcgHFYFY", "mobina.kochakniayahho.com", "hiiii", 1, "aaa");
 		assertEquals(SignupAndProfileMenuMessage.INVALID_EMAIL, error);
 	}
 
@@ -93,7 +93,7 @@ public class AppTest {
 		StrongHold.getUsers().add(new User("gcdyuhg", "eywy",
 				"Mobina_Kochaknia34", "Mobina_Kochaknia34", "mobina.kochaknia@yahho.com", 0, 1, "hiiii"));
 		SignupAndProfileMenuMessage error = SignupMenuController.signup("dasdas", "faad", "wSdd77*das",
-				"mobina.kochaknia@yahho.com", "hjf");
+				"mobina.kochaknia@yahho.com", "hjf", 1, "aaa");
 		assertEquals(SignupAndProfileMenuMessage.EMAIL_EXIST, error);
-	}*/
+	}
 }
