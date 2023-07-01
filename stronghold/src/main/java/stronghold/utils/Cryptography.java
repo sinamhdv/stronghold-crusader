9a7c99da913f1a96128744249ae6ecd55c4b8dad
sinamhdv <sinamhdv@gmail.com>
2023-07-01 22:03:37 +0330

    completed jwt

--- stronghold/src/main/java/stronghold/utils/Cryptography.java
+++ stronghold/src/main/java/stronghold/utils/Cryptography.java
@@ -13,5 +13,18 @@ public class Cryptography {
 	public static String generateJWT(String username) {
 		String jsonData = "{\"username\":\"" + username + "\"}";
 		String encodedData = Base64.encodeBase64String(jsonData.getBytes());
+		String hash = hashPassword(encodedData + JWT_SECRET);
+		return encodedData + "." + hash;
+	}
+
+	public static boolean verifyJWT(String jwt) {
+		try {
+			String[] splitToken = jwt.split("\\.");
+			return (splitToken.length == 2 &&
+				hashPassword(splitToken[0] + JWT_SECRET).equals(splitToken[1]));
+		}
+		catch (Exception ex) {
+			return false;
+		}
 	}
 }
